7b18ca4961d278dae2f27d1e3e1ef36f5f496c5b
Koushik Panchadarla <koushikpanchadarla@gmail.com>
2026-01-23 12:19:39 +0530

    feat: bootstrap Win32 window in x86 assembly

---
+++ src/main.asm
@@ -0,0 +1,88 @@
+.386
+.model flat, stdcall
+option casemap:none
+
+include \masm32\include\windows.inc
+include \masm32\include\kernel32.inc
+include \masm32\include\user32.inc
+include \masm32\include\gdi32.inc
+
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\user32.lib
+includelib \masm32\lib\gdi32.lib
+
+WinMain PROTO :DWORD, :DWORD, :DWORD, :DWORD
+WndProc  PROTO :DWORD, :DWORD, :DWORD, :DWORD
+
+.const
+WINDOW_CLASS db "OpcodeArenaWnd", 0
+WINDOW_TITLE db "Opcode Arena", 0
+
+.code
+start:
+    invoke GetModuleHandle, NULL
+    invoke WinMain, eax, NULL, NULL, SW_SHOWDEFAULT
+    invoke ExitProcess, eax
+
+WinMain PROC hInst:DWORD, hPrev:DWORD, lpCmd:DWORD, nShow:DWORD
+    LOCAL wc:WNDCLASSEX
+    LOCAL msg:MSG
+    LOCAL hwnd:DWORD
+
+    mov wc.cbSize, SIZEOF WNDCLASSEX
+    mov wc.style, CS_HREDRAW or CS_VREDRAW
+    mov wc.lpfnWndProc, OFFSET WndProc
+    mov wc.cbClsExtra, 0
+    mov wc.cbWndExtra, 0
+    mov eax, hInst
+    mov wc.hInstance, eax
+    mov wc.hbrBackground, COLOR_WINDOW+1
+    mov wc.lpszMenuName, NULL
+    mov wc.lpszClassName, OFFSET WINDOW_CLASS
+
+    invoke LoadIcon, NULL, IDI_APPLICATION
+    mov wc.hIcon, eax
+    mov wc.hIconSm, eax
+    invoke LoadCursor, NULL, IDC_ARROW
+    mov wc.hCursor, eax
+
+    invoke RegisterClassEx, ADDR wc
+
+    invoke CreateWindowEx, 0,
+        ADDR WINDOW_CLASS,
+        ADDR WINDOW_TITLE,
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600,
+        NULL, NULL,
+        hInst, NULL
+
+    mov hwnd, eax
+    invoke ShowWindow, hwnd, SW_SHOWNORMAL
+    invoke UpdateWindow, hwnd
+
+msg_loop:
+    invoke GetMessage, ADDR msg, NULL, 0, 0
+    cmp eax, 0
+    je exit_loop
+    invoke TranslateMessage, ADDR msg
+    invoke DispatchMessage, ADDR msg
+    jmp msg_loop
+
+exit_loop:
+    mov eax, msg.wParam
+    ret
+WinMain ENDP
+
+WndProc PROC hWnd:DWORD, uMsg:DWORD, wParam:DWORD, lParam:DWORD
+    .if uMsg == WM_DESTROY
+        invoke PostQuitMessage, 0
+        xor eax, eax
+        ret
+    .endif
+
+    invoke DefWindowProc, hWnd, uMsg, wParam, lParam
+    ret
+WndProc ENDP
+
+END start
